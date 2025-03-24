51fba43d1dbc093f046664383bd977e43f01aafa
rajagaurdevops <your.rajagaur333@gmail.com>
2025-03-24 16:28:38 +0530

    hello-world

---
+++ hello-world/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Author - raja kumar Gaur"
+
+echo "🚀 Installation script started..."
+echo "✅ Running updates..."
+echo     "apt-get update"
+
+echo "✅ Installing necessary packages..."
+echo  "apt-get install -y curl"
+
+echo "✅ Installation complete!"
+echo "Hello, World!"
