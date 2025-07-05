e9f16d6ef1df5c2d3e6b94d0b7203a2a70959d13
rajagaurdevops <rajagaur333@gmail.com>
2025-07-05 11:52:08 +0000

    testing

--- basic-python-app/app.py
+++ basic-python-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask App deployed successfully via GitHub Actions!  Auther - Raja kumar Gaur"
+    return "Flask App deployed successfully via GitHub Actions!  Auther - Raja kumar Gaur and aman and nishant"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
